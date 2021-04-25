0a049f59207f505bfbb0d3952f07797cdbd30f4b
ZarrarPalekar <zarrar@live.in>
2021-04-26 03:39:01 +0530

    UI Fix

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
 
+import { Button } from "react-bootstrap";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-  VerticalTimelineElementProps,
-  VerticalTimelineProps,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 
 import "./projects-timeline.styles.css";
 
-import SchoolIcon from "@material-ui/icons/School";
 import WorkIcon from "@material-ui/icons/Work";
 import StarIcon from "@material-ui/icons/Star";
 import { myProjects } from "../../data/myProjects";
 
 const TimeLine = () => {
+  // const history = useHistory();
   return (
     <div id="projects">
       <VerticalTimeline>
@@ -28,7 +26,6 @@ const TimeLine = () => {
           return (
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
-              date="2010 - 2011"
               iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
               icon={<WorkIcon />}
               date={project.date}
@@ -39,18 +36,19 @@ const TimeLine = () => {
               <h4 className="vertical-timeline-element-subtitle">
                 {project.techUsed}
               </h4>
-              <p>
-                <strong>Application Website: </strong>
-                <a href={project.appUrl}>{project.appUrl}</a>
-              </p>
-              <p>
-                <strong>Source Code: </strong>
-                <a href={project.gitUrl}>{project.gitUrl}</a>
-              </p>
+              <Button
+                className="mt-2 mr-2"
+                variant="success"
+                href={project.appUrl}
+              >
+                View Website
+              </Button>
+              <Button className="mt-2" variant="warning" href={project.gitUrl}>
+                Source Code
+              </Button>
             </VerticalTimelineElement>
           );
         })}
-
         <VerticalTimelineElement
           iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
           icon={<StarIcon />}
