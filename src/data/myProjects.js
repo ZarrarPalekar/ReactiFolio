a4dac938bc2deb871b64893b2219f18ab4401cda
ZarrarPalekar <zarrar@live.in>
2021-04-26 03:07:46 +0530

    Add a project for test

--- src/data/myProjects.js
+++ src/data/myProjects.js
@@ -6,4 +6,11 @@ export const myProjects = [
     gitUrl: "https://github.com/ZarrarPalekar/ProShop",
     date: "Mar-2021",
   },
+  {
+    projectName: "Dev Conn App",
+    techUsed: "MERN, Redux",
+    appUrl: "http://devconn.zarrarpalekar.com/",
+    gitUrl: "https://github.com/ZarrarPalekar/SocialDevApp",
+    date: "Nov-2020",
+  },
 ];
