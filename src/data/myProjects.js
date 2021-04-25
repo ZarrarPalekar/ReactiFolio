6a62773f6570516b5cf4cda206b3cb74ef42f042
Zarrar Palekar <zarrar@live.in>
2021-04-26 03:02:31 +0530

    Test Remove

--- src/data/myProjects.js
+++ src/data/myProjects.js
@@ -5,12 +5,5 @@ export const myProjects = [
     appUrl: "https://zarrareshopapp.herokuapp.com/",
     gitUrl: "https://github.com/ZarrarPalekar/ProShop",
     date: "Mar-2021",
-  },
-  {
-    projectName: "Dev Conn App",
-    techUsed: "MERN, Redux",
-    appUrl: "http://devconn.zarrarpalekar.com/",
-    gitUrl: "https://github.com/ZarrarPalekar/SocialDevApp",
-    date: "Nov-2020",
-  },
+  }
 ];
