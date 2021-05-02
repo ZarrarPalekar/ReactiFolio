8a97f53275d91b5186f39f1e09efd52275ba4da3
ZarrarPalekar <zarrar@live.in>
2021-05-02 15:01:18 +0530

    Add WA Fix +

--- src/components/whatsapp-me/whatsapp.component.jsx
+++ src/components/whatsapp-me/whatsapp.component.jsx
@@ -5,7 +5,7 @@ const Whatsapp = () => {
   return (
     <div>
       <a
-        href="https://api.whatsapp.com/send?phone=00919867363215&text=Hi%20Zarrar%21%20I%20came%20here%20from%20your%20portfolio%20website."
+        href="https://api.whatsapp.com/send?phone=+919867363215&text=Hi%20Zarrar%21%20I%20came%20here%20from%20your%20portfolio%20website."
         class="float"
         target="_blank"
         rel="noopener noreferrer"
