32c646dc115617c9d88a489ebc4d08795c93b89f
ZarrarPalekar <zarrar@live.in>
2021-05-17 00:25:03 +0530

    Add Experiences

--- src/data/myProjects.js
+++ src/data/myProjects.js
@@ -1,16 +1,58 @@
 export const myProjects = [
   {
     projectName: "E-Shop App",
-    techUsed: "MERN, Redux",
+    techUsed: "MERN, Redux, JWT",
     appUrl: "https://zarrareshopapp.herokuapp.com/",
     gitUrl: "https://github.com/ZarrarPalekar/ProShop",
     date: "Mar-2021",
   },
   {
     projectName: "Dev Conn App",
-    techUsed: "MERN, Redux",
+    techUsed: "MERN, Redux, JWT",
     appUrl: "http://devconn.zarrarpalekar.com/",
     gitUrl: "https://github.com/ZarrarPalekar/SocialDevApp",
     date: "Nov-2020",
+  },
+  {
+    projectName: "Dev Conn App",
+    techUsed: "MERN, JWT",
+    appUrl: "http://shoppinglist.zarrarpalekar.com/",
+    date: "Oct-2020",
+  },
+  {
+    projectName: "My Blogs",
+    techUsed: "EJS, MongoDB",
+    appUrl: "http://blog.zarrarpalekar.com/",
+    date: "Sep-2020",
+  },
+  {
+    projectName: "My Newsletter",
+    techUsed: "ReactJS, Mailchimp API",
+    appUrl: "http://newsletter.zarrarpalekar.com/",
+    date: "Sep-2020",
+  },
+  {
+    projectName: "Secrets App",
+    techUsed: "EJS, MongoDB, Google OAuth",
+    appUrl: "http://secrets.zarrarpalekar.com/",
+    date: "Aug-2020",
+  },
+  {
+    projectName: "Todo List",
+    techUsed: "HTML, CSS, Bootstrap, EJS",
+    appUrl: "http://todo.zarrarpalekar.com/",
+    date: "Aug-2020",
+  },
+  {
+    projectName: "Portfolio Website",
+    techUsed: "ReactJS, StyledComponents, Github Pages",
+    appUrl: "https://www.zarrarpalekar.com/",
+    date: "Jul-2020",
+  },
+  {
+    projectName: "Tindog - Tinder for dogs",
+    techUsed: "HTML, CSS, Bootstrap",
+    appUrl: "http://tindog.zarrarpalekar.com/",
+    date: "Jun-2020",
   },
 ];
